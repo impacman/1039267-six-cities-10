6e400b4de5fa145b79f948a457adb7563ee224f1
pacman <pacman.0396@gmail.com>
2022-07-30 13:29:19 +0300

    Разделение верстки на отдельные страницы и создание нового компонента для карточек

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,36 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
+import FavoritesEmptyScreen from '../../pages/favorites-empty-screen/favorites-empty-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import MainEmptyScreen from '../../pages/main-empty-screen/main-empty-screen';
+import MainScreen from '../../pages/main-screen/main-screen';
+import PropertyNotLoggedScreen from '../../pages/property-not-logged-screen/property-not-logged-screen';
+import PropertyScreen from '../../pages/property-screen/property-screen';
+
+type AppScreenProps = {
+  cards: {
+    id: number,
+    premium: boolean,
+    img: string,
+    price: number,
+    rating: number,
+    name: string,
+    type: string,
+    bookmark: boolean
+  }[],
+};
+
+function App({cards}: AppScreenProps): JSX.Element {
+  return (
+    <>
+      <MainScreen cards={cards} />
+      {/* <FavoritesEmptyScreen /> */}
+      {/* <FavoritesScreen /> */}
+      {/* <LoginScreen /> */}
+      {/* <MainEmptyScreen /> */}
+      {/* <PropertyNotLoggedScreen /> */}
+      {/* <PropertyScreen /> */}
+    </>
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Settings = {
+  CARDS: [
+    {id: 1, premium: true, img: 'apartment-01.jpg', price: 120, rating: 80, name: 'Beautiful &amp; luxurious apartment at great location', type: 'Apartment', bookmark: false},
+    {id: 2, premium: false, img: 'room.jpg', price: 80, rating: 80, name: 'Wood and stone place', type: 'Private room', bookmark: true},
+    {id: 3, premium: false, img: 'apartment-02.jpg', price: 132, rating: 80, name: 'Canal View Prinsengracht', type: 'Apartment', bookmark: false},
+    {id: 4, premium: true, img: 'apartment-03.jpg', price: 180, rating: 100, name: 'Nice, cozy, warm big bed apartment', type: 'Apartment', bookmark: false},
+    {id: 5, premium: false, img: 'room.jpg', price: 80, rating: 80, name: 'Wood and stone place', type: 'Private room', bookmark: false},
+  ]
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App cards={Settings.CARDS} />
   </React.StrictMode>,
 );
